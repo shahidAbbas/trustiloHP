c6d1221175701ede23aa80f6cca8d3621af083df
amar.mishra <amar.mishra@appventurez.com>
2025-08-20 18:19:47 +0530

    ARA-1255 : additional padding on top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,24 @@ import AnimatedIcon from "@/components/AnimatedIcon";
 
 export default function ConfirmationPage() {
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen bg-white pt-16 pb-0">
-      <div className="mb-6"><AnimatedIcon /></div>
+    <>
+      <div className="flex flex-col items-center justify-start min-h-screen bg-white pb-0"
+        style={{ paddingTop: 'calc(var(--spacing) * 16 + 116px)' }}
+      >
+        <div className="mb-6"><AnimatedIcon /></div>
 
-      <span className="text-lg font-medium text-gray-900">
-        Ready for Confirmation
-      </span>
+        <span className="text-lg font-medium text-gray-900">
+          Ready for Confirmation
+        </span>
 
-      <button
-        className="mt-4 px-6 py-2 border border-[#1C69D4]
+        <button
+          className="mt-4 px-6 py-2 border border-[#1C69D4]
              text-[16px] leading-6 font-medium text-[#1C69D4] font-barlow
              rounded-[4px] bg-[#1C69D440]"
-      >
-        Trusted Channel Online
-      </button>
-    </div>
+        >
+          Trusted Channel Online
+        </button>
+      </div>
+    </>
   );
 }
